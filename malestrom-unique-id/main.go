3ace4ddac3dbd97851803de3f39df9c9a62241c0
vidyaprabhakar <vidyap23@gmail.com>
2023-06-03 00:11:12 -0400

    trying out challenge 2- unique-id generation

---
+++ malestrom-unique-id/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+    "encoding/json"
+    "log"
+	"time"
+	"fmt"
+	
+    maelstrom "github.com/jepsen-io/maelstrom/demo/go"
+)
+
+func main(){
+	node := maelstrom.NewNode()
+	node.Handle("generate", func(msg maelstrom.Message) error {
+		// Unmarshal the message body as an loosely-typed map.
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
+	     
+		// Update the message type to return back.
+		currentTime := time.Now()
+		str := fmt.Sprintf("%v", body["msg_id"])
+		msg_id := str + currentTime.Format("2006-01-02")
+		body["type"] = "generate_ok"
+		body["id"] = msg_id
+	
+		// Echo the original message back with the updated message type.
+		return node.Reply(msg, body)
+	})
+	if err := node.Run(); err != nil {
+		log.Fatal(err)
+	}
+}
